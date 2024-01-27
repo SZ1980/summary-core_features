ea429fc12bea6e4cb31e92bcc3e5f2e5f5e279ab
Zoran Simonovic <simonovic1980@gmail.com>
2024-01-27 17:17:38 +0100

    form submission and list of posts

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
-import MainHeader from "./components/MainHeader";
 import PostsList from "./components/PostsList";
+import MainHeader from "./components/MainHeader";
 
 function App() {
   const [modalIsVisible, setModalIsVisible] = useState(false);
@@ -9,6 +9,7 @@ function App() {
   function showModalHandler() {
     setModalIsVisible(true);
   }
+
   function hideModalHandler() {
     setModalIsVisible(false);
   }

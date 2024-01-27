97e2c0578b2ee634316ef090b76d645863f64942
Zoran Simonovic <simonovic1980@gmail.com>
2024-01-27 14:02:19 +0100

    children prop and conditional rendering

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -2,24 +2,34 @@ import { useState } from "react";
 
 import Post from "./Post";
 import NewPost from "./NewPost";
+import Modal from "./Modal";
 import classes from "./PostsList.module.css";
 
 function PostsList() {
+  const [modalIsVisible, setModalIsVisible] = useState(true);
   const [enteredBody, setEnteredBody] = useState("");
   const [enteredAuthor, setEnteredAuthor] = useState("");
+
   function bodyChangeHandler(event) {
     setEnteredBody(event.target.value);
   }
   function authorChangeHandler(event) {
     setEnteredAuthor(event.target.value);
   }
+  function hideModalHandler() {
+    setModalIsVisible(false);
+  }
 
   return (
     <>
-      <NewPost
-        onBodyChange={bodyChangeHandler}
-        onAuthorChange={authorChangeHandler}
-      />
+      {modalIsVisible && (
+        <Modal onClose={hideModalHandler}>
+          <NewPost
+            onBodyChange={bodyChangeHandler}
+            onAuthorChange={authorChangeHandler}
+          />
+        </Modal>
+      )}
       <ul className={classes.posts}>
         <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Maximilian" body="React.js is awesome!" />
